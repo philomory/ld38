6698ae5014e8b6149fca718e88c464aa5b44e04b
Adam Gardner <adambg@gmail.com>
2017-04-24 15:41:22 -1000

    Added MacOS distribution version

---
+++ dist/macos/ruby-app/main.rb
@@ -0,0 +1,12 @@
+require 'gosu'
+require 'require_all'
+require 'crack'
+
+APP_ROOT = File.dirname(__FILE__)
+SOURCE_ROOT = File.join(APP_ROOT,'src')
+MEDIA_ROOT = File.join(APP_ROOT,'media')
+DATA_ROOT = File.join(APP_ROOT,'data')
+
+require_all File.join(File.dirname(__FILE__),'src')
+
+Game.play!
