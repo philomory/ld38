08ea9c87548438c18647ad9f8034a54bc1d873ff
Adam Gardner <adambg@gmail.com>
2017-06-03 17:36:45 -1000

    Allow rocks to be pushed over keys after all.

--- src/props/pickup.rb
+++ src/props/pickup.rb
@@ -15,9 +15,9 @@ class Pickup < Prop
   # TODO: Re-evaluate; is this intuitive? If not, what benefit does it have for things to work this way?
   # Is it worth making the rules more complicated for the sake of only _one_ level that uses that complication?
   # Even if I like that level a lot?
-  def passable?(passer)
-    passer.is_a?(Enemy::Stone) ? false : super
-  end
+  #def passable?(passer)
+  #  passer.is_a?(Enemy::Stone) ? false : super
+  #end
   
   def pickup!
     raise NotImplementedError, "Subclasses must implemenet :pickup!"
