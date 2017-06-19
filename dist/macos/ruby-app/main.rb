24bcff6e61b2b6ce531496b7d97f51c6ac9dd5a5
Adam Gardner <adambg@gmail.com>
2017-06-19 06:39:02 -1000

    Fix Mac bundle main.rb

--- dist/macos/ruby-app/main.rb
+++ dist/macos/ruby-app/main.rb
@@ -6,7 +6,7 @@ APP_ROOT = File.dirname(__FILE__)
 SOURCE_ROOT = File.join(APP_ROOT,'src')
 MEDIA_ROOT = File.join(APP_ROOT,'media')
 DATA_ROOT = File.join(APP_ROOT,'data')
-SETTINGS_FILE = File.expand_path("../../..",APP_ROOT) #TODO: Should be user's Library/Preferences maybe?
+SETTINGS_FILE = File.expand_path("../../../settings.yml",APP_ROOT) #TODO: Should be user's Library/Preferences maybe?
 
 require_all File.join(File.dirname(__FILE__),'src')
 
